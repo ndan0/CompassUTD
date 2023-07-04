03aeed38b98977c2b0d5e74ad9f4a91749a47cf0
Dan Nguyen <ndan0112@gmail.com>
2023-07-03 22:57:15 -0500

    Init ai serving, summarize scrape content
    Added llm to summarize content scrapped
    Added AI serving endpoint

--- endpointtest.py
+++ endpointtest.py
@@ -1,15 +1,23 @@
 import requests
-
+#Test Access
 url = "http://127.0.0.1:8000/access/"
 #params = {"url": "https://dox.utdallas.edu/syl120000"}
-#params = {"url": "https://catalog.utdallas.edu/2018/undergraduate/courses/cs1336"}
+#params = {"url": "https://catalog.utdallas.edu/2018/undergraduate/courses/acct2302"}
 #params = {"url": "https://catalog.utdallas.edu/2023/undergraduate/home"}
-params = {"url": "https://catalog.utdallas.edu/2022/undergraduate/courses/math2419/makepdf"}
+#params = {"url": "https://catalog.utdallas.edu/2022/undergraduate/courses/hist2301"}
+#params = {"url": "https://engineering.utdallas.edu/academics/undergraduate-majors/undergrad-advising/ecs-advisors/"}
+#params = {"url": "https://jindal.utdallas.edu/advising/"}
 
 response = requests.get(url, params=params)
 
+# #Test Inference
+# url = "https://compass-utd-serving-api-z4khdeirua-uc.a.run.app/inference/"
+# params = {"token": "", "user_message": "Hello World!"}
+
+# response = requests.post(url, params=params)
+
 if response.status_code == 200:
-    data = response.json().get("data")
+    data = response.json()
     print(data)
 else:
     print("Error occurred while accessing URL.")
