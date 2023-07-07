fbd5b1687271e7dbbea34d937c79b7ec99157e96
Dan Nguyen <ndan0112@gmail.com>
2023-07-06 23:41:09 -0500

    Update endpointtest.py

--- endpointtest.py
+++ endpointtest.py
@@ -1,21 +1,22 @@
 import requests
-#Test Access
-url = "http://127.0.0.1:8000/access/"
-#params = {"url": "https://dox.utdallas.edu/syl120000"}
-#params = {"url": "https://catalog.utdallas.edu/2018/undergraduate/courses/acct2302"}
-#params = {"url": "https://catalog.utdallas.edu/2023/undergraduate/home"}
-#params = {"url": "https://catalog.utdallas.edu/2022/undergraduate/courses/hist2301"}
-#params = {"url": "https://engineering.utdallas.edu/academics/undergraduate-majors/undergrad-advising/ecs-advisors/"}
+
+# Test Access
+#url = "http://127.0.0.1:8000/access/"
+# params = {"url": "https://dox.utdallas.edu/syl120000"}
+# params = {"url": "https://catalog.utdallas.edu/2018/undergraduate/courses/acct2302"}
+# params = {"url": "https://catalog.utdallas.edu/2023/undergraduate/home"}
+# params = {"url": "https://catalog.utdallas.edu/2022/undergraduate/courses/hist2301"}
+# params = {"url": "https://engineering.utdallas.edu/academics/undergraduate-majors/undergrad-advising/ecs-advisors/"}
 #params = {"url": "https://jindal.utdallas.edu/advising/"}
 
-response = requests.get(url, params=params)
+#response = requests.get(url, params=params)
 
-# #Test Inference
-# url = "https://compass-utd-serving-api-z4khdeirua-uc.a.run.app/inference/"
-# params = {"token": "", "user_message": "Hello World!"}
+#Test Inference
+url = "http://127.0.0.1:8000/inference/"
+params = {"token": "", "user_message": "What is Schedule Planner and How do I enroll in to class?"}
 
-# response = requests.post(url, params=params)
-
+response = requests.post(url, params=params)
+print(response)
 if response.status_code == 200:
     data = response.json()
     print(data)
